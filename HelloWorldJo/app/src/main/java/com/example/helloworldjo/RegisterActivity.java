eebf963043a4d6d14076a02aa0b3a5b9185bd36b
Joette Damo <jcdamo@gmail.com>
2020-04-28 23:30:18 -0700

    comment form 2

---
+++ HelloWorldJo/app/src/main/java/com/example/helloworldjo/RegisterActivity.java
@@ -0,0 +1,10 @@
+package com.example.helloworldjo;
+
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText signupInputName, SignupInputEmail, signupInputUsername, signupInputDob;
+
+}
